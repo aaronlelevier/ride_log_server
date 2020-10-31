f85db5b0d1f93cfabcd12b11d642c06ba0ce2b81
aaronlelevier <aaron.lelevier@gmail.com>
2020-10-31 08:28:51 -0700

    'cancellation check' is scheduled based on the allowed 'registration time'

--- apps/rl/src/rl_race.erl
+++ apps/rl/src/rl_race.erl
@@ -15,9 +15,9 @@
 %% API
 -export([start_link/2, get_state/1, cancel/1, register_rider/2, stop/1]).
 %% State callbacks
--export([registration/3]).
+-export([registration/3, cancelled/3]).
 %% gen_statem callbacks
--export([init/1, format_status/2, handle_event/4, terminate/3, code_change/4,
+-export([init/1, format_status/2, terminate/3, code_change/4,
          callback_mode/0]).
 
 %%------------------------------------------------------------------------------
@@ -37,7 +37,7 @@
                    prepare_for_start_time => seconds(),
                    race_time => seconds(),
                    points => points(),
-                   cancellation_ref => timer:tref() | undefined}.
+                   cancellation_check_ref => reference() | undefined}.
 
 %%%===================================================================
 %%% API
@@ -77,16 +77,28 @@ registration({call, From}, {register_rider, Rider}, State) ->
         State#{rider_count := maps:get(rider_count, State) + 1,
                riders := [Rider | maps:get(riders, State)]},
     {keep_state, NewState, [{reply, From, {registration, State}}]};
+registration({call, From}, get_state, State) ->
+    {keep_state, State, [{reply, From, {registration, State}}]};
 registration(EventType, EventContent, State) ->
     handle_event(EventType, EventContent, State).
 
-handle_event({call, From}, get_state, State) ->
-    {keep_state, State, [{reply, From, {registration, State}}]};
+cancelled({call, From}, get_state, State) ->
+    {keep_state, State, [{reply, From, {cancelled, State}}]};
+cancelled(EventType, EventContent, State) ->
+    handle_event(EventType, EventContent, State).
 
-%% TODO: either need to check the `From` here or need another way to differentiate between a 'force cancel' and a 'cancel per min_riders not present'
 handle_event({call, From}, cancel, State) ->
     lager:debug("~p", [{{call, From}, cancel, State}]),
     {next_state, cancelled, State, [{reply, From, {cancelled, State}}]};
+
+handle_event({call, _From}, {cancellation_check, Ref}, #{cancellation_check_ref := Ref}=State) ->
+    lager:debug("~p", [{{call, _From}, {cancellation_check, Ref}, State}]),
+    case maps:get(rider_count, State) < maps:get(min_rider_count, State) of
+        true ->
+            {next_state, cancelled, State};
+        false ->
+            {keep_state, State}
+    end;
 
 handle_event(EventType, EventContent, State) ->
     lager:debug("Unhandled event: ~p", [{EventType, EventContent, State}]),
@@ -103,7 +115,7 @@ handle_event(EventType, EventContent, State) ->
 -spec init(state()) -> {ok, registration, state()}.
 init(State) ->
     lager:debug("~p", [State]),
-    {ok, State2} = set_cancellation_policy(State),
+    {ok, State2} = set_cancellation_check(State),
     {ok, registration, State2}.
 
 %% @private
@@ -123,16 +135,9 @@ format_status(_Opt, [_PDict, _StateName, _State]) ->
     Status.
 
 %% @private
-%% @doc If callback_mode is handle_event_function, then whenever a
-%% gen_statem receives an event from call/2, cast/2, or as a normal
-%% process message, this function is called.
-handle_event(_EventType, _EventContent, _StateName, State) ->
-    NextStateName = the_next_state_name,
-    {next_state, NextStateName, State}.
-
-%% @private
 %% @doc This function is called by a gen_statem when it is about to terminate
-terminate(_Reason, _StateName, _State) ->
+terminate(Reason, StateName, State) ->
+    lager:debug("~p", [{Reason, StateName, State}]),
     ok.
 
 %% @private
@@ -144,10 +149,14 @@ code_change(_OldVsn, StateName, State, _Extra) ->
 %%% Internal functions
 %%%===================================================================
 
--spec set_cancellation_policy(state()) -> {ok, state()}.
-set_cancellation_policy(State) ->
+-spec set_cancellation_check(state()) -> {ok, state()}.
+set_cancellation_check(State) ->
     {registered_name, Name} = process_info(self(), registered_name),
-    %% TODO: no sure if `send_after` should be used instead?
-    {ok, TRef} =
-        timer:apply_after(maps:get(registration_time, State), ?MODULE, cancel, [Name]),
-    {ok, State#{cancellation_ref => TRef}}.
+    Ref = make_ref(),
+    {ok, _TRef} =
+        timer:apply_after(
+            maps:get(registration_time, State) * 1000,
+            gen_statem,
+            call,
+            [Name, {cancellation_check, Ref}]),
+    {ok, State#{cancellation_check_ref => Ref}}.
--- test/unit/rl/rl_race_tests.erl
+++ test/unit/rl/rl_race_tests.erl
@@ -18,19 +18,15 @@ a_race_can_be_started_and_cancelled_test() ->
   {ok, _Pid} = rl_race:start_link(Race, Args),
 
   % initial state
-  {StateName1, State1} = rl_race:get_state(Race),
-
+  {StateName1, _State1} = rl_race:get_state(Race),
   ?assertEqual(registration, StateName1),
-  ?assertEqual(Args, State1),
 
   % cancel race
-  {StateName2, State2} = rl_race:cancel(Race),
-
-  ?assertEqual(cancelled, StateName2),
-  ?assertEqual(Args, State2).
+  {StateName2, _State2} = rl_race:cancel(Race),
+  ?assertEqual(cancelled, StateName2).
 
 race_cancelled_if_N_riders_not_registered_in_T_time_test() ->
-  RegistrationTime = 1,
+  RegistrationTime = 0,
   Race = race1,
   Args = #{
     rider_count => 0,
@@ -45,13 +41,9 @@ race_cancelled_if_N_riders_not_registered_in_T_time_test() ->
   {ok, _Pid} = rl_race:start_link(Race, Args),
 
   % initial state
-  {StateName1, _State1} = rl_race:get_state(Race),
-  ?assertEqual(registration, StateName1),
-
-  timer:sleep(RegistrationTime * 1000),
-
-  {StateName2, _State2} = rl_race:get_state(Race),
-  ?assertEqual(cancelled, StateName2).
+  {StateName1, State} = rl_race:get_state(Race),
+  true = maps:get(rider_count, State) < maps:get(min_rider_count, State),
+  ?assertEqual(cancelled, StateName1).
 
 
 %%%===================================================================
@@ -73,7 +65,8 @@ register_rider_test_() ->
     fun register_rider_cleanup/1,
     [
       fun start_with_0_riders/0,
-      fun register_1_rider/0
+      fun register_1_rider/0,
+      fun min_rider_count_satisfied_so_race_not_cancelled/0
     ]
   }.
 
@@ -94,6 +87,20 @@ register_1_rider() ->
   ?assertEqual(1, maps:get(rider_count, State2)),
   ?assertEqual([Rider], maps:get(riders, State2)).
 
+min_rider_count_satisfied_so_race_not_cancelled() ->
+  {StateName, State} = rl_race:get_state(?RACE),
+  ?assertEqual(registration, StateName),
+  ?assertEqual(
+    true,
+    maps:get(rider_count, State) >= maps:get(min_rider_count, State)
+  ),
+
+  %% TODO: might be a better way than sleeping for the registration time to confirm check
+  timer:sleep(maps:get(registration_time, State) * 1000),
+
+  {StateName, State} = rl_race:get_state(?RACE),
+  ?assertEqual(registration, StateName).
+
 
 %%%===================================================================
 %%% Internal functions
@@ -103,7 +110,7 @@ args() -> #{
   riders => [],
   min_rider_count => 1,
   max_rider_count => 2,
-  registration_time => 2,
+  registration_time => 1,
   prepare_for_start_time => 2,
   race_time => 10,
   points => 3
